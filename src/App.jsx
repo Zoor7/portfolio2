1cb49281dc6b871ab600c517b071c5bc9d6be70b
Hector <hectorllorca7@gmail.com>
2022-04-03 18:38:30 +0200

    about me & footer

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,14 @@ import Language from './sections/LanguagesSection/Language'
 
 import './App.css'
 
-
-
 function App() {
-  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+  const [theme, setTheme] = useLocalStorage(
+    'theme',
+    defaultDark ? 'dark' : 'light'
+  )
 
-  const switchTheme =()=>{
+  const switchTheme = () => {
     const newTheme = theme === 'light' ? 'dark' : 'light'
     setTheme(newTheme)
   }
@@ -23,10 +24,11 @@ function App() {
       <Sidebar theme={theme} switchTheme={switchTheme}></Sidebar>
       <div className="main">
         <div className="main-container">
-
           <div className="about-me">
             <h2>ABOUT ME</h2>
-            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum, cumque maiores perspiciatis accusantium porro numquam? Numquam ipsum, non dolores, at nostrum, sequi et obcaecati incidunt commodi asperiores accusantium optio laudantium? Amet natus perspiciatis doloremque optio voluptatum. Labore molestiae in obcaecati rem? Aut dolores impedit, autem non deleniti quam animi ex illo a eius similique blanditiis odio error provident voluptas dolorum facilis distinctio suscipit, recusandae ipsum. Repudiandae tenetur ullam cumque autem ea eius totam pariatur, perspiciatis possimus ut repellat minus animi quis natus doloremque corporis nobis nulla consequuntur dicta mollitia perferendis aspernatur! Tempora aliquam quibusdam possimus, perferendis animi iste provident ex!</div>
+            <div>
+              <p>Hi, I'm Héctor, I like to spend time clicking buttons and creating things.</p>
+            </div>
           </div>
           <div className="studies">
             <h2>STUDIES</h2>
@@ -36,7 +38,17 @@ function App() {
             </div>
             <div>
               <h4>FullStack Bootcamp</h4>
-              <p>-<a className='link' href='https://www.fullstackopen.com/en' target="_blank" rel='noopener noreferrer'  >fullstackopen.com/en</a></p>
+              <p>
+                -
+                <a
+                  className="link"
+                  href="https://www.fullstackopen.com/en"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  fullstackopen.com/en
+                </a>
+              </p>
             </div>
           </div>
           <div className="experience">
@@ -48,17 +60,14 @@ function App() {
             <div>
               <h4>Looking for an opportunity</h4>
             </div>
-
           </div>
           <Projects></Projects>
           <div className="footer">
             <Contact></Contact>
             <Language></Language>
-
           </div>
         </div>
       </div>
-
     </div>
   )
 }
--- src/sections/ProjectsSection/Projects.jsx
+++ src/sections/ProjectsSection/Projects.jsx
@@ -1,38 +1,33 @@
 import { useState } from 'react'
-import Card from "../../Components/Card/Card"
-import "./projects.css"
-import data from "../../utils/data.json"
-
-
+import Card from '../../Components/Card/Card'
+import './projects.css'
+import data from '../../utils/data.json'
 
 const Projects = () => {
+  const [project, setProject] = useState()
 
-    const [project, setProject] = useState();
+  const handleClick = (click) => {
+    setProject(data[click.target.innerText])
+  }
 
-    const handleClick = (click) => {
-        setProject(data[click.target.innerText])
-    }
-
-    const handleClose = () => {
-        return (
-            setProject('')
-        )
-    }
-    return (
-        <div className='projects'>
-            <h2>PROJECTS</h2>
-            {project ?
-                <div>
-                    <Card project={project} handleClose={handleClose}></Card>
-                </div>
-                : <ul>
-                    <li onClick={handleClick}>BasketRank</li>
-                    <li onClick={handleClick}>aerolab-challenge-vite</li>
-                </ul>
-            }
-
+  const handleClose = () => {
+    return setProject('')
+  }
+  return (
+    <div className="projects">
+      <h2>PROJECTS</h2>
+      {project ? (
+        <div>
+          <Card project={project} handleClose={handleClose}></Card>
         </div>
-    );
+      ) : (
+        <ul>
+          <li onClick={handleClick}>BasketRank</li>
+          <li onClick={handleClick}>aerolab-challenge-vite</li>
+        </ul>
+      )}
+    </div>
+  )
 }
 
-export default Projects;
+export default Projects
