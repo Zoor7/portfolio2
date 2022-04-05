705d72e25be9cdaa047a2dd3009796291f81c4e4
Hector <hectorllorca7@gmail.com>
2022-04-05 18:39:38 +0200

    title animations, better layout

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,22 +1,20 @@
 import React from 'react'
-import "./card.css"
+import './card.css'
 
 const Card = ({ project, handleClose }) => {
-    return (
-        <div className='card'>
-            <div  className='close'>
-                <span onClick={handleClose}>X</span>
-            </div>
-            <div className='card-image'>
-                <a href={project.link} target="_blank" rel='noreferrer'>
-                    <img src={project.img} alt="" />
-                </a>
-
-            </div>
-            <div className="description">{project.descripcion}</div>
-
-        </div>
-    );
+  return (
+    <div className="card">
+      <div className="close">
+        <span onClick={handleClose}>X</span>
+      </div>
+      <div className="card-image">
+        <a href={project.link} target="_blank" rel="noreferrer">
+          <img src={project.img} alt="" />
+        </a>
+      </div>
+      <div className="description">{project.descripcion}</div>
+    </div>
+  )
 }
 
-export default Card;
+export default Card
--- src/sections/Sidebar/sidebar.jsx
+++ src/sections/Sidebar/sidebar.jsx
@@ -1,44 +1,37 @@
-import React from 'react';
-import ThemeSwitch from '../../Components/ThemeSwitch/ThemeSwitch';
-import Contact from '../Contact/Contact';
-import Language from '../LanguagesSection/Language';
-import "./sidebar.css"
+import React from 'react'
+import ThemeSwitch from '../../Components/ThemeSwitch/ThemeSwitch'
+import Contact from '../Contact/Contact'
+import Language from '../LanguagesSection/Language'
+import './sidebar.css'
 
-
-const Sidebar = ({switchTheme,theme}) => {
-
-
-    return (
-
-        <div className="sidebar">
-
-            <div className="avatar">
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg" />
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" />
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" />
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" />
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg" />
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
-                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg" />
-            </div>
-            <div className="name-role">
-                <div className="name">Héctor LLorca</div>
-                <div className="role">Junior FullStack</div>
-            <ThemeSwitch theme={theme} switchTheme={switchTheme}></ThemeSwitch>
-            </div>
-            <hr />
-            <div className="lan-contact">
-                <Language></Language>
-                <Contact></Contact>
-            </div>
-
-        </div>
-
-    );
+const Sidebar = ({ switchTheme, theme }) => {
+  return (
+    <div className="sidebar">
+      <div className="avatar">
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg" />
+      </div>
+      <div className="name-role">
+        <div className="name">Héctor LLorca</div>
+        <div className="role">Junior FullStack</div>
+        <ThemeSwitch theme={theme} switchTheme={switchTheme}></ThemeSwitch>
+      </div>
+      <hr />
+      <div className="lan-contact">
+        <Language></Language>
+        <Contact></Contact>
+      </div>
+    </div>
+  )
 }
 
-export default Sidebar;
+export default Sidebar
