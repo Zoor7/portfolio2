ffe0bec2b2d68b24f9eec64b97325f97c0082676
Zoor <hectorllorca7@gmail.com>
2022-04-20 18:15:45 +0200

    a

--- src/Components/cvDownload/cvDownload.jsx
+++ src/Components/cvDownload/cvDownload.jsx
@@ -5,7 +5,7 @@ import cvpaper from '../../assets/cv-hector.pdf'
 const CvDownload = () => {
   return (
     <div className="cvDownload">
-      <a href={cvpaper} download={cvpaper}>
+      <a href={cvpaper} download="cv-hector-llorca">
         <img src={cv} alt="cv" />
       </a>
     </div>
--- src/sections/Sidebar/sidebar.jsx
+++ src/sections/Sidebar/sidebar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Avatar from '../../Components/avatar/avatar'
 import CvDownload from '../../Components/cvDownload/cvDownload'
 import ThemeSwitch from '../../Components/ThemeSwitch/ThemeSwitch'
 import Contact from '../Contact/Contact'
@@ -8,19 +9,7 @@ import './sidebar.css'
 const Sidebar = ({ switchTheme, theme }) => {
   return (
     <div className="sidebar">
-      <div className="avatar">
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" />
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg" />
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" />
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" />
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" />
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg" />
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg" />
-      </div>
+        <Avatar/>
       <div className="name-role">
         <p className="name">Héctor LLorca</p>
         <p className="role">Junior FullStack</p>
