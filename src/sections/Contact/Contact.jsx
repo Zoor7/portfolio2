47799b406a2a7b9123885fa55860c2c57dc6e82a
Zoor <hectorllorca7@gmail.com>
2022-04-20 19:46:21 +0200

    a

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -1,18 +1,21 @@
-
-import "./Contact.css"
+import './Contact.css'
 
 const Contact = () => {
-    return ( 
-        <div className="contact">
-        <h2>Contact</h2>
-        <ul>
-            <li>Barcelona</li>
-            <li>hectorllorca7@gmail.com</li>
-            <a href="http://github.com/Zoor7" target="_blank" rel='noreferrer'><li> github.com/Zoor7</li></a>
-        </ul>
+  return (
+    <div className="contact">
+      <h2>Contact</h2>
+      <ul>
+        <li>Barcelona</li>
+        <li>hectorllorca7@gmail.com</li>
+        <a href="http://github.com/Zoor7" target="_blank" rel="noreferrer">
+          <li> github/zoor7</li>
+        </a>
+        <a href="https://www.linkedin.com/in/hector-llorca/" target="_blank" rel="noreferrer">
+          <li>linkedin</li>
+        </a>
+      </ul>
     </div>
-
-     );
+  )
 }
- 
-export default Contact;
+
+export default Contact
